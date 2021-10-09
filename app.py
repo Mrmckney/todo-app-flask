e032e278267f470365d3c3c48d9227c7ab0ab7dc
Matthew McKney <mrmckney@gmail.com>
2021-10-09 18:56:32 -0400

    Added user login and signup

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+from enum import unique
 from flask import Flask, redirect, url_for, render_template, request
+from flask.globals import session
+from flask.helpers import flash
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.engine import create_engine
 
 # Flask
 app = Flask(__name__)
@@ -10,18 +14,21 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG'] = True
+engine = create_engine('sqlite:///book.sqlite')
 # db.init_app(app)
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     todo = db.Column(db.String(120))
+    date = db.Column(db.String())
     completed = db.Column(db.Boolean)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(45))
-    password = db.Column(db.String(26))
+    email = db.Column(db.String(45), unique=True, nullable=False)
+    password = db.Column(db.String(26), unique=True, nullable=False)
+    
 
 @app.route("/")
 def home_page():
@@ -32,10 +39,26 @@ def todo_page():
     todo_list = Todo.query.all()
     return render_template("todo.html", todo_list=todo_list)
 
+@app.route("/date")
+def todo_date():
+    todo_list = Todo.query.order_by(Todo.date).all()
+    return render_template("todo.html", todo_list=todo_list)
+
+@app.route("/completed")
+def todo_completed():
+    todo_list = Todo.query.order_by(Todo.completed.desc()).all()
+    return render_template("todo.html", todo_list=todo_list)
+
+@app.route("/notcompleted")
+def todo_notcompleted():
+    todo_list = Todo.query.order_by(Todo.completed).all()
+    return render_template("todo.html", todo_list=todo_list)
+
 @app.route("/add", methods=["POST"])
 def add_todo():
     todo = request.form.get("todo")
-    new_todo = Todo(todo=todo, completed=False)
+    todo_date = request.form.get("date")
+    new_todo = Todo(todo=todo, date=todo_date ,completed=False)
     db.session.add(new_todo)
     db.session.commit()
     return redirect(url_for("todo_page"))
@@ -61,6 +84,39 @@ def login_page():
 @app.route("/signup")
 def sign_up_page():
     return render_template("signup.html")
+
+@app.route("/register/", methods=["GET", "POST"])
+def register():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if request.method == "POST":
+        new_user = User(email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        session["logged_in"] = True 
+        return render_template("todo.html") and redirect(url_for("todo_page"))
+    else:
+        return flash("Try Again")
+
+@app.route("/login/", methods = ["GET", "POST"])
+def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if request.method == "POST":
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if (user.password, password):
+                session["logged_in"] = True 
+                return render_template("todo.html") and redirect(url_for("todo_page"))
+            else:
+                flash("Username or Password Incorrect")
+                return redirect(url_for("login_page"))
+    return render_template("todo.html")
+
+@app.route("/logout/")
+def logout():
+    session["logged_in"] = False
+    return redirect(url_for("home_page"))
 
 
 if __name__ == "__main__":
