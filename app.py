11aa9e16c0ba6e73f5984b908cca269cbe2e0587
Matthew McKney <mrmckney@gmail.com>
2021-10-09 21:26:20 -0400

    added css

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
-from enum import unique
 from flask import Flask, redirect, url_for, render_template, request
 from flask.globals import session
 from flask.helpers import flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.engine import create_engine
 
-# Flask
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '\x14B~^\x07\xe1\x197\xda\x18\xa6[[\x05\x03QVg\xce%\xb2<\x80\xa4\x00'
 app.config['DEBUG'] = True
 
-# Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG'] = True
 engine = create_engine('sqlite:///book.sqlite')
-# db.init_app(app)
+
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
